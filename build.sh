300b9221524da4213cb0c86d8b931b790942d34c
Seth Gorelik <srgorelik@users.noreply.github.com>
2025-03-13 08:17:54 -0500

    rename

--- build.sh
+++ build.sh
@@ -22,10 +22,10 @@ IMAGE_TAG='v1.0'
 docker build --platform=linux/amd64 --tag "${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}" .
 
 # create GCP artifact registry repository (only need to run once)
-# gcloud beta artifacts repositories create ${REPO_NAME} --repository-format=docker --location=${REGION} --project=${PROJECT_ID}
+gcloud beta artifacts repositories create ${REPO_NAME} --repository-format=docker --location=${REGION} --project=${PROJECT_ID}
 
 # add permission in order to push the image (only need to run once)
-# gcloud auth configure-docker "${REGION}-docker.pkg.dev" --project=${PROJECT_ID}
+gcloud auth configure-docker "${REGION}-docker.pkg.dev" --project=${PROJECT_ID}
 
 # push the image to the created repo
 docker push "${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}"
